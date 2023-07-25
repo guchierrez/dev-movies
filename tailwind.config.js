97c2bbe43a9de1ee0ef879f8a0f9dfff06768793
unknown <marcelinogs2001@gmail.com>
2023-07-25 10:38:53 -0300

    set project settings

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FFBB38",
+          "base-100": "#3D3D3D",
+          "base-200": "#171717",
+          "base-300": "000000",
+        },
+      },
+    ],
+  },
+};
